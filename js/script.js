0249da0ca4494b88abdbac04040528c7406710e2
Litvinenko Boris <boris12lit@gmail.com>
2018-05-12 16:43:48 +0300

    Сделал секцию с сертификатами

--- js/script.js
+++ js/script.js
@@ -72,6 +72,8 @@ $( document ).ready(function() {
   });
 
 
+
+
   // делаю слайдер для мобильной версии этапов работы
 
   checkMedia(); // запускаем чек окна при открытии страницы
@@ -96,6 +98,37 @@ $( document ).ready(function() {
       });
       }
     }
+
+    $('.sertification__container').slick({
+  dots: false,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 1279,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    // надо запомнить и попробовать эту фичу
+  ]
+  });
 });
 
   // Слайдер наши материалы
